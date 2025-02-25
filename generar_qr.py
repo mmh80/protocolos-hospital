8ad99233ee7acb9c5e06481be9bb97d3e8cccc75
MMH80 <mauel@picparks.org>
2025-02-25 17:09:41 -0300

    Subir códigos QR actualizados con enlaces de descarga directa

--- generar_qr.py
+++ generar_qr.py
@@ -1,30 +1,16 @@
 import qrcode
 
-# Lista de URLs de los archivos PDF
-urls = [
-    'https://raw.githubusercontent.com/mmh80/protocolos-hospital/main/Criterios_Ingreso_Mantencion_Egreso_Pacientes_UPC.pdf',
-    'https://raw.githubusercontent.com/mmh80/protocolos-hospital/main/Monitoreo_Hemodinamico_Invasivo_No_Invasivo.pdf',
-    'https://raw.githubusercontent.com/mmh80/protocolos-hospital/main/Norma_IAAS_C_Difficile_2024.pdf',
-    'https://raw.githubusercontent.com/mmh80/protocolos-hospital/main/PC_Analgesia_Sedacion_BNM_UPC.pdf',
-    'https://raw.githubusercontent.com/mmh80/protocolos-hospital/main/Prono_Paciente_Critico_Adulto.pdf',
-    'https://raw.githubusercontent.com/mmh80/protocolos-hospital/main/Protocolo_Instalacion_Manejo_Enfermeria_Linea_Arterial.pdf',
-    'https://raw.githubusercontent.com/mmh80/protocolos-hospital/main/Protocolo_Manejo_Linea_Arterial.pdf'
+archivos = [
+    ("Criterios_Ingreso_Mantencion_Egreso_Pacientes_UPC", "https://github.com/mmh80/protocolos-hospital/raw/main/Criterios_Ingreso_Mantencion_Egreso_Pacientes_UPC.pdf"),
+    ("Monitoreo_Hemodinamico_Invasivo_No_Invasivo", "https://github.com/mmh80/protocolos-hospital/raw/main/Monitoreo_Hemodinamico_Invasivo_No_Invasivo.pdf"),
+    ("Norma_IAAS_C_Difficile_2024", "https://github.com/mmh80/protocolos-hospital/raw/main/Norma_IAAS_C_Difficile_2024.pdf"),
+    ("PC_Analgesia_Sedacion_BNM_UPC", "https://github.com/mmh80/protocolos-hospital/raw/main/PC_Analgesia_Sedacion_BNM_UPC.pdf"),
+    ("Prono_Paciente_Critico_Adulto", "https://github.com/mmh80/protocolos-hospital/raw/main/Prono_Paciente_Critico_Adulto.pdf"),
+    ("Protocolo_Instalacion_Manejo_Enfermeria_Linea_Arterial", "https://github.com/mmh80/protocolos-hospital/raw/main/Protocolo_Instalacion_Manejo_Enfermeria_Linea_Arterial.pdf"),
+    ("Protocolo_Manejo_Linea_Arterial", "https://github.com/mmh80/protocolos-hospital/raw/main/Protocolo_Manejo_Linea_Arterial.pdf")
 ]
 
-# Generar los códigos QR para cada URL
-for i, url in enumerate(urls, start=1):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
-
-    # Crear una imagen del QR y guardarla con un nombre único
-    img = qr.make_image(fill='black', back_color='white')
-    img.save(f'codigo_qr_{i}.png')
-
-print("Códigos QR generados correctamente.")
+for nombre, url in archivos:
+    qr = qrcode.make(url)
+    qr.save(f"{nombre}.png")
 
